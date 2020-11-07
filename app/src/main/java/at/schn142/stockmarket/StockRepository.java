233caa8455beb8c894ebea68b063c8eae6b57765
michaelschneider93 <schneidermichael@outlook.com>
2020-11-07 23:19:25 +0100

    fit layout to fragment

--- app/src/main/java/at/schn142/stockmarket/StockRepository.java
+++ app/src/main/java/at/schn142/stockmarket/StockRepository.java
@@ -63,6 +63,7 @@ class StockRepository {
         new deleteStockAsyncTask(mStockDao).execute(stock);
     }
 
+    //TODO replace with thread
     private static class deleteAllStocksAsyncTask extends AsyncTask<Void, Void, Void> {
         private StockDao mAsyncTaskDao;
 
@@ -77,6 +78,7 @@ class StockRepository {
         }
     }
 
+    //TODO replace with thread
     private static class deleteStockAsyncTask extends AsyncTask<Stock, Void, Void> {
         private StockDao mAsyncTaskDao;
 
